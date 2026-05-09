9c28497772d0a623f1863a8a614fab3c14f58187
Ahmed-Asid <ahmedasid249@gmail.com>
2026-05-09 19:09:55 +0600

    added 'force dynamic' in signin page

--- src/app/(auth)/sign-in/page.jsx
+++ src/app/(auth)/sign-in/page.jsx
@@ -3,6 +3,7 @@ import SignIn from './SignIn';
 
 export const dynamic = "force-dynamic";
 
+
 export const metadata = {
     title: 'SkillSphere - Sign in',
     description: 'Login with your account',
